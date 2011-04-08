d55bcd02946ed96adba55e0fea9cc112309d4e3d
FND <fnd@fnd-oda.(none)>
2011-04-08 08:09:17 +0200

    added various annotations and clarifications

--- basics.py
+++ basics.py
@@ -1,8 +1,12 @@
 # primitives
 
-my_string = "..." # can also be enclosed in single quotes; triple quotes for multiline
+my_string = "..." # can also be enclosed in single quotes
+my_multiline_string = """
+lorem ipsum
+dolor sit amet
+"""
 my_unicode_string = u"..."
-my_raw_string = r"..."
+my_raw_string = r"..." # e.g. for RegEx patterns
 my_int = 1
 my_float = 1.0 # NB: `1 / 2` != `1.0 / 2`
 success, failure = True, False
@@ -13,24 +17,30 @@ success, failure = True, False
 my_list = ["foo", "bar", "baz"]
 my_list = my_list + ["lorem", "ipsum", "dolor", "sit", "amet"]
 my_list.append("...")
+print len(my_list) # 9
 
 first = my_list[0]
 last = my_list[-1]
-my_slice = my_list[2:7:2]
+my_slice = my_list[2:7:2] # ['baz', 'ipsum', 'sit']
 print my_slice
-my_selection = [item.upper() for item in my_list if item.endswith("r")]
+my_selection = [item.upper() for item in my_list if item.endswith("r")] # ['BAR', 'DOLOR']
 print my_selection
+# slicing works for strings too
+my_slice = my_list[1:] # ['baz', 'ipsum', 'sit']
+print my_slice
 
 
 # tuples: like lists, but immutable
 
-my_tuple = ("foo",)
-#my_tuple[1] = "bar" # TypeError exception
+my_tuple = ("foo", "bar")
+#my_tuple[2] = "baz" # TypeError exception
+my_tuple = tuple(my_list) # cf. list, dict etc.
 
 
 # sets: unordered sequence, contains no duplicates - cf. frozenset (immutable)
 
 my_set = set(my_list)
+# mathematical operations: intersection, union, difference etc.
 
 
 # generators: lazy sequence
@@ -48,7 +58,13 @@ my_dict = {
 
 print my_dict.keys(), my_dict.values(), my_dict.items()
 
-my_dict = dict([('foo', 'lorem ipsum'), ('bar', 'dolor sit amet')])
+for key, value in my_dict.items():
+    print "%s: %s" % (key, value)
+
+my_dict = dict([
+    ('foo', 'lorem ipsum'),
+    ('bar', 'dolor sit amet')
+])
 
 
 # functions
@@ -59,7 +75,7 @@ def do_something(foo, bar=None): # supports *args & **kwargs -- NB: beware of mu
     """
     return foo
 
-sqr = lambda x: x * x
+sqr = lambda x: x * x # anonymous function
 print sqr(3)
 
 def my_wrapper(msg):
@@ -90,7 +106,7 @@ my_instance = FooBar("...")
 my_instance.do_something()
 
 
-# exceptions
+# exceptions - "ask for forgiveness, not permission"
 
 try:
     [][0]
@@ -119,10 +135,9 @@ for i, item in enumerate(["foo", "bar", "baz"]):
 # utilities
 
 print dir(my_list)
-help(my_dict)
+help(my_dict) # ~ pydoc
 print do_something.__doc__
 
 # misc
 # * `pass`
 # * line continuation
-# * CPython, Jython, IronPython, PyPy
